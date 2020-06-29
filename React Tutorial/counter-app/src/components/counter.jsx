f4c9bed17662dad14c9a17504373239ec54a847b
Truc Phan <anhtrucphanit@gmail.com>
2020-06-29 12:51:55 +0700

    Commit

---
+++ React Tutorial/counter-app/src/components/counter.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+class Counter extends Component {
+    state = {
+        count: 0,
+        tags: ["tag1", "tag2", "tag3"],
+    };
+
+    handleIncrement = () => {
+        this.setState({ count: this.state.count + 1 });
+    };
+
+    doHandleIncrement = () =>{
+        this.handleIncrement({id: 1})
+    }
+    // renderTags() {
+    //     if (this.state.tags.length === 0) return <p>There are no tags :D</p>;
+    //     return (
+    //         <ul>
+    //             {this.state.tags.map((tag) => (
+    //                 <li key={tag}>{tag}</li>
+    //             ))}
+    //         </ul>
+    //     );
+    // }
+
+    // render() {
+    //     return (
+    //         <>
+    //         <div>
+    //             {this.state.tags.length === 0 && "Please create a new tag!"}
+    //             {this.renderTags()}
+    //         </div>
+    //         <button >Increment</button>
+    //         </>
+    //     );
+    // }
+    render() {
+        console.log('props', this.props);
+        return (
+            <div>
+                <span className={this.getBadgeClasses()}>
+                    {this.formatCount()}
+                </span>
+                <button
+                    onClick={() => this.handleIncrement()}
+                    className="btn btn-secondary btn-sm"
+                >
+                    Increment
+                </button>               
+            </div>
+        );
+    }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const count = this.state.count;
+        return count === 0 ? "Zero" : count;
+    }
+}
+
+export default Counter;
---
+++ React Tutorial/hello-world/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+function App() {
+  return (
+    <div className="App">
+      <Greet />
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
