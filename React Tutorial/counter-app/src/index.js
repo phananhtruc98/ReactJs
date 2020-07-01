5cf2637f193e20cb2fee89bafea32b4f445d32d3
Truc Phan <anhtrucphanit@gmail.com>
2020-07-01 13:37:48 +0700

    Multiple Components in Sync + Lifting the State up

--- React Tutorial/counter-app/src/index.js
+++ React Tutorial/counter-app/src/index.js
@@ -5,8 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css'; 
 import Counter from './components/counter';
-import Counters from './components/counters';
-ReactDOM.render( <Counters />,
+ReactDOM.render( <App />,
   document.getElementById('root')
 );
 
