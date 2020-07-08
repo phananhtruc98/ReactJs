31c495e6b2e2fc2f2c5f9112bb7a5ac8f44ac570
Truc Phan <anhtrucphanit@gmail.com>
2020-07-08 17:36:08 +0700

    Commit

--- React Tutorial/todo-list/src/App.js
+++ React Tutorial/todo-list/src/App.js
@@ -6,26 +6,45 @@ class App extends Component {
 
   state = {
     todos: [
-      { id: 1, 
-        title: 'Take out the trash', 
+      {
+        id: 1,
+        title: 'Take out the trash',
         completed: false
       },
-      { id: 2, 
-        title: 'Do homework', 
+      {
+        id: 2,
+        title: 'Do homework',
         completed: false
-      }, 
-      { id: 3, 
-        title: 'Play with cat', 
+      },
+      {
+        id: 3,
+        title: 'Play with cat',
         completed: true
       }
     ]
   }
 
+// Toggle complete
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    });
+  }
+
+  delTodo = (id) => {
+    console.log(id)
+  }
+
   render() {
     return (
-  
+
       <div className="App" >
-        <Todos todos={this.state.todos}/>
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
       </div>
     );
   }
--- React Tutorial/todo-list/src/components/TodoItem.js
+++ React Tutorial/todo-list/src/components/TodoItem.js
@@ -3,19 +3,24 @@ import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
 
-    getStyle = () =>{
+    getStyle = () => {
         return {
             background: 'pink',
             padding: '5px',
             border: '1px black solid',
-            textDecoration: this.props.todo.completed ?  'line-through':'none'
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
         }
     }
 
     render() {
+        const { id, title} = this.props.todo;
         return (
             <div style={this.getStyle()}>
-                <p>{this.props.todo.title}</p>
+                <p>
+                    <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} />{''}
+                    {title}
+                    <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
+                </p>
             </div>
         )
     }
@@ -27,4 +32,13 @@ TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
 }
 
+const btnStyle = {
+    background: '#ff0000',
+    color: '#fff',
+    border: 'none', 
+    padding: '5px 10px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
+}
 export default TodoItem
